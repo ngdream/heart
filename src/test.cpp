3a52395062ca05c1f0d14874c8a395d3f61a1561
ngdream <nguewofoss@gmail.com>
2022-10-02 00:48:27 +0100

    add

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,16 @@
 #include "heart.h"
 
+class Test : public View
+{
+    Httpresponse get(Request request)
+    {
+        throw Http404();
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-
+    Url::add(new Test, "/yo", "yo");
     Server::run();
     return 0;
 }
